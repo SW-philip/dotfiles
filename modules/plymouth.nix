a5b0e735c33b6c5ad5e1785e3f729c967ddeeccb
Phil Repko <philipjrepko@gmail.com>
2026-05-17 21:25:00 -0400

    boot splash removal

--- modules/plymouth.nix
+++ modules/plymouth.nix
@@ -5,13 +5,4 @@
 
   boot.initrd.verbose = false;
 
-  boot.plymouth = {
-    enable = false;
-    theme = "colorful";
-    themePackages = [
-      (pkgs.adi1090x-plymouth-themes.override {
-        selected_themes = [ "colorful" ];
-      })
-    ];
-  };
 }
