21ec22cf45e63625f7219c160d2265a0d0908ced
Phil Repko <philipjrepko@gmail.com>
2026-05-17 21:51:11 -0400

    boot splash removal

--- hosts/surface/boot.nix
+++ hosts/surface/boot.nix
@@ -9,9 +9,10 @@
   boot.initrd.kernelModules = [ "i915" ];
 
   boot.kernelParams = lib.mkAfter [
-    "quiet" "splash" "loglevel=0"
+    "quiet" "loglevel=0"
     "rd.systemd.show_status=false"
     "systemd.show_status=false"
     "vt.handoff=7"
+    "delayacct"
   ];
 }
