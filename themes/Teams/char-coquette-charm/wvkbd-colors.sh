092cecc3e935bd4c072d85fa469b53575b56da3c
Philip Repko <philipjrepko@gmail.com>
2026-05-21 07:53:47 -0400

    themes

---
+++ themes/Teams/char-coquette-charm/wvkbd-colors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# wvkbd color args for char - Coquette Charm
+WVKBD_ARGS="--bg f1bbca --fg eea5b5 --fg-sp e98595 --press e96d82 --press-sp e92060 --swipe c34b61 --swipe-sp c95c45 --text 1a1a1a --text-sp d1426a"
---
+++ themes/Teams/charred-charred-cerulean/wvkbd-colors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# wvkbd color args for charred - Charred Cerulean
+WVKBD_ARGS="--bg 1f3a61 --fg 244a76 --fg-sp 285c8b --press 4493d5 --press-sp 59a5b1 --swipe 84d6e1 --swipe-sp 80b5e5 --text ffffff --text-sp 5783c2"
