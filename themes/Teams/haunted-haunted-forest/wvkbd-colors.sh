498ba417fb6860ee4c9aaff174b2961969b5c45b
Phil Repko <philipjrepko@gmail.com>
2026-05-22 14:07:40 -0400

    new themes

---
+++ themes/Teams/haunted-haunted-forest/wvkbd-colors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# wvkbd color args for haunted - Haunted Forest
+WVKBD_ARGS="--bg 2b3d50 --fg 395c70 --fg-sp 437c8f --press 4c6d90 --press-sp b8c6b9 --swipe d9d9d9 --swipe-sp dbd8d7 --text ffffff --text-sp 6d89a7"
